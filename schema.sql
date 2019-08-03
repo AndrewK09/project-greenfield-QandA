000082bfef5ed313b6776ca612e5f6016e9c32a3
Andrew <Andrewkn09@Gmail.com>
2019-08-02 22:57:12 -0400

    Add default values for answer and update primary key query

--- schema.sql
+++ schema.sql
@@ -25,14 +25,14 @@ SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), tru
 -- DROP TABLE IF EXISTS public."answers";
 -- CREATE TABLE public."answers"
 -- (
---     answer_id integer NOT NULL,
+--     answer_id serial,
 --     question_id integer,
 --     body character varying(1000) COLLATE pg_catalog."default",
 --     date date DEFAULT CURRENT_DATE,
 --     answerer_name character varying(60) COLLATE pg_catalog."default",   
 --     answerer_email character varying(60) COLLATE pg_catalog."default",
---     report integer,
---     helpfulness integer,
+--     report integer DEFAULT 0,
+--     helpfulness integer DEFAULT 0,
 --     CONSTRAINT "Answers_pkey" PRIMARY KEY (answer_id)
 -- )
 -- WITH (
@@ -40,6 +40,7 @@ SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), tru
 -- );
 
 -- \COPY  answers FROM 'csv/answers.csv' DELIMITER ',' CSV HEADER;
+-- SELECT setval('answers_answer_id_seq', (SELECT count(*) from answers), true); 
 
 -- ALTER TABLE answers
 -- ADD COLUMN photos jsonb[] DEFAULT '{}'::jsonb[];
