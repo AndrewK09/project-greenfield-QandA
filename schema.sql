debac08f159495aef1c79855c16586339375307a
Andrew <Andrewkn09@Gmail.com>
2019-08-02 23:47:25 -0400

    Uncomment code

--- schema.sql
+++ schema.sql
@@ -1,50 +1,51 @@
 
--- DROP TABLE IF EXISTS "questions";
--- CREATE TABLE questions
--- (
---     question_id SERIAL,
---     product_id integer NOT NULL,
---     question_body character varying(1000) COLLATE pg_catalog."default" NOT NULL,
---     question_date date DEFAULT CURRENT_DATE,
---     asker_name character varying(60) COLLATE pg_catalog."default" NOT NULL,
---     asker_email character varying(60) COLLATE pg_catalog."default" NOT NULL,
---     reported integer DEFAULT 0,
---     question_helpfulness integer DEFAULT 0,
---     CONSTRAINT "Questions_pkey" PRIMARY KEY (question_id)
--- )
--- WITH (
---     OIDS = FALSE
--- );
+DROP TABLE IF EXISTS "questions";
+CREATE TABLE questions
+(
+    question_id SERIAL,
+    product_id integer NOT NULL,
+    question_body character varying(1000) COLLATE pg_catalog."default" NOT NULL,
+    question_date date DEFAULT CURRENT_DATE,
+    asker_name character varying(60) COLLATE pg_catalog."default" NOT NULL,
+    asker_email character varying(60) COLLATE pg_catalog."default" NOT NULL,
+    reported integer DEFAULT 0,
+    question_helpfulness integer DEFAULT 0,
+    CONSTRAINT "Questions_pkey" PRIMARY KEY (question_id)
+)
+WITH (
+    OIDS = FALSE
+);
 
--- \COPY questions FROM 'csv/questions.csv' DELIMITER ',' CSV HEADER;
+\COPY questions FROM 'csv/questions.csv' DELIMITER ',' CSV HEADER;
+SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), true); 
 
--- SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), true); 
+-- ===========================   ANSWERS    ==============================
 
--- DROP TABLE IF EXISTS public."answers";
--- CREATE TABLE public."answers"
--- (
---     answer_id serial,
---     question_id integer,
---     body character varying(1000) COLLATE pg_catalog."default",
---     date date DEFAULT CURRENT_DATE,
---     answerer_name character varying(60) COLLATE pg_catalog."default",   
---     answerer_email character varying(60) COLLATE pg_catalog."default",
---     report integer DEFAULT 0,
---     helpfulness integer DEFAULT 0,
---     CONSTRAINT "Answers_pkey" PRIMARY KEY (answer_id)
--- )
--- WITH (
---     OIDS = FALSE
--- );
+DROP TABLE IF EXISTS answers;
+CREATE TABLE answers
+(
+    answer_id serial,
+    question_id integer,
+    body character varying(1000) COLLATE pg_catalog."default",
+    date date DEFAULT CURRENT_DATE,
+    answerer_name character varying(60) COLLATE pg_catalog."default",   
+    answerer_email character varying(60) COLLATE pg_catalog."default",
+    report integer DEFAULT 0,
+    helpfulness integer DEFAULT 0,
+    CONSTRAINT "Answers_pkey" PRIMARY KEY (answer_id)
+)
+WITH (
+    OIDS = FALSE
+);
 
--- \COPY  answers FROM 'csv/answers.csv' DELIMITER ',' CSV HEADER;
--- SELECT setval('answers_answer_id_seq', (SELECT count(*) from answers), true); 
+\COPY  answers FROM 'csv/answers.csv' DELIMITER ',' CSV HEADER;
+SELECT setval('answers_answer_id_seq', (SELECT count(*) from answers), true); 
 
--- ALTER TABLE answers
--- ADD COLUMN photos jsonb[] DEFAULT '{}'::jsonb[];
+ALTER TABLE answers
+ADD COLUMN photos jsonb[] DEFAULT '{}'::jsonb[];
 
--- DROP TABLE IF EXISTS photos;
-
+-- ===========================   PHOTOS    ==============================
+DROP TABLE IF EXISTS photos;
 CREATE TABLE photos
 (
     id serial,
