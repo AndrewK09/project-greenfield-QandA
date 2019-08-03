59ca2e38027c84beb527ebd98ab76453b72f0b44
Andrew <Andrewkn09@Gmail.com>
2019-08-02 23:40:03 -0400

    Add photos table

--- schema.sql
+++ schema.sql
@@ -1,26 +1,24 @@
 
-DROP TABLE IF EXISTS "questions";
-CREATE TABLE questions
-(
-    question_id SERIAL,
-    product_id integer NOT NULL,
-    question_body character varying(1000) COLLATE pg_catalog."default" NOT NULL,
-    question_date date DEFAULT CURRENT_DATE,
-    asker_name character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    asker_email character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    reported integer DEFAULT 0,
-    question_helpfulness integer DEFAULT 0,
-    CONSTRAINT "Questions_pkey" PRIMARY KEY (question_id)
-)
-WITH (
-    OIDS = FALSE
-);
+-- DROP TABLE IF EXISTS "questions";
+-- CREATE TABLE questions
+-- (
+--     question_id SERIAL,
+--     product_id integer NOT NULL,
+--     question_body character varying(1000) COLLATE pg_catalog."default" NOT NULL,
+--     question_date date DEFAULT CURRENT_DATE,
+--     asker_name character varying(60) COLLATE pg_catalog."default" NOT NULL,
+--     asker_email character varying(60) COLLATE pg_catalog."default" NOT NULL,
+--     reported integer DEFAULT 0,
+--     question_helpfulness integer DEFAULT 0,
+--     CONSTRAINT "Questions_pkey" PRIMARY KEY (question_id)
+-- )
+-- WITH (
+--     OIDS = FALSE
+-- );
 
-\COPY questions FROM 'csv/questions.csv' DELIMITER ',' CSV HEADER;
+-- \COPY questions FROM 'csv/questions.csv' DELIMITER ',' CSV HEADER;
 
-SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), true); 
-
-
+-- SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), true); 
 
 -- DROP TABLE IF EXISTS public."answers";
 -- CREATE TABLE public."answers"
@@ -44,3 +42,20 @@ SELECT setval('questions_question_id_seq', (SELECT count(*) from questions), tru
 
 -- ALTER TABLE answers
 -- ADD COLUMN photos jsonb[] DEFAULT '{}'::jsonb[];
+
+-- DROP TABLE IF EXISTS photos;
+
+CREATE TABLE photos
+(
+    id serial,
+    answer_id integer NOT NULL,
+    url text COLLATE pg_catalog."default",
+    CONSTRAINT photos_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+\COPY photos from 'csv/answers_photos.csv' DELIMITER ',' CSV HEADER;
+
+SELECT setval('photos_id_seq', (SELECT count(*) from photos), true); 
