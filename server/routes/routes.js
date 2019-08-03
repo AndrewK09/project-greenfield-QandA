958dd9fe7801eb9098f4fe0ee1b78ae1711eecab
Andrew <Andrewkn09@Gmail.com>
2019-08-03 09:52:12 -0400

    Create put routes for question answers

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -4,8 +4,12 @@ const answerController = require('../controllers/answerController.js');
 
 router.get('/:product_id/:page?/:count?', questionController.getQuestions);
 router.post('/:product_id', questionController.addQuestion);
+router.put('/question/:question_id/helpful', questionController.markHelpful);
+router.put('/question/:question_id/report', questionController.report);
 
 router.get('/:question_id/answers/:page?/:count?', answerController.getAnswers);
 router.post('/:question_id/answers', answerController.addAnswer);
+router.put('/answer/:answer_id/helpful', answerController.markHelpful);
+router.put('/answer/:answer_id/report', answerController.report);
 
 module.exports = router;
