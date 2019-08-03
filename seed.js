de0310de0f74d5757d22591ea44e12e08ff71fb0
Andrew <Andrewkn09@Gmail.com>
2019-08-03 08:51:48 -0400

    Refactor to promises

--- seed.js
+++ seed.js
@@ -10,17 +10,20 @@ fs.createReadStream(path.join(__dirname, './csv/answers_photos.csv'), options)
   .pipe(csv.parse({ headers: true }))
   .on('data', row => {
     let photo = JSON.stringify({ id: row.id, url: row[' url'] });
-    db.query(
-      `UPDATE answers SET photos = $2::jsonb || photos WHERE answer_id = $1;`,
-      [row[' answer_id'], photo],
-      (err, result) => {
-        if (err) console.log(err);
+    return db
+      .any(
+        `UPDATE answers SET photos = $2::jsonb || photos   WHERE answer_id = $1;`,
+        [row[' answer_id'], photo]
+      )
+      .then(() => {
         totalCount++;
-        if ((totalCount % 100, 000 === 0)) {
+        if (totalCount % 100000 === 0) {
           console.log(totalCount);
         }
-      }
-    );
+      })
+      .catch(error => {
+        console.log(error);
+      });
   })
   .on('end', () => {
     console.log('Completed adding photos');
@@ -28,26 +31,3 @@ fs.createReadStream(path.join(__dirname, './csv/answers_photos.csv'), options)
   .on('err', err => {
     console.log(err);
   });
-
-// fs.createReadStream(path.join(__dirname, './csv/answers_photos.csv'), options)
-//   .pipe(csv.parse({ headers: true }))
-//   .on('data', row => {
-//     let photo = JSON.stringify({ id: row.id, url: row[' url'] });
-//     pool.query(
-//       `UPDATE answers SET photos = photos || $2::jsonb WHERE answer_id = $1;`,
-//       [row[' answer_id'], photo],
-//       (err, result) => {
-//         if (err) console.log(err);
-//         totalCount++;
-//         if ((totalCount % 100, 000 === 0)) {
-//           console.log(totalCount);
-//         }
-//       }
-//     );
-//   })
-//   .on('end', () => {
-//     console.log('Completed adding photos');
-//   })
-//   .on('err', err => {
-//     console.log(err);
-//   });
