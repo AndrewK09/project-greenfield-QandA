cd024f81e3ed4b745f370b43b7f9ed53c6af1dc8
Andrew <Andrewkn09@Gmail.com>
2019-08-02 23:48:01 -0400

    Finish addAnswer model

--- server/models/answerModel.js
+++ server/models/answerModel.js
@@ -9,9 +9,30 @@ module.exports = {
     );
   },
   addAnswer: (question_id, { body, name, email, photos }) => {
-    return db.any(
-      `INSERT INTO answers (question_id, question_body, asker_name, asker_email) VALUES ($1, $2, $3, $4)`,
-      [question_id, body, name, email]
-    );
+    return db
+      .one(
+        `INSERT INTO answers (question_id, body, answerer_name, answerer_email) VALUES ($1, $2, $3, $4) RETURNING answer_id`,
+        [question_id, body, name, email]
+      )
+      .then(({ answer_id }) => {
+        return Promise.all(
+          photos.map(url => {
+            //insert the url and return the id
+            //insert into answer photos object containing returned id and url
+            return db
+              .one(
+                `INSERT INTO photos (answer_id, url) VALUES ($1, $2) RETURNING id;`,
+                [answer_id, url]
+              )
+              .then(({ id }) => {
+                let photo = JSON.stringify({ id, url });
+                return db.any(
+                  `UPDATE answers SET photos = photos || $1::jsonb WHERE answer_id = $2;`,
+                  [photo, answer_id]
+                );
+              });
+          })
+        );
+      });
   },
 };
