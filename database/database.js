000ed15ce86610a1b7b097da3f93b4b5489fa3ee
Andrew <Andrewkn09@Gmail.com>
2019-08-05 16:17:05 -0400

    Update package

--- database/database.js
+++ database/database.js
@@ -1,6 +1,15 @@
 const pgp = require('pg-promise')();
 
-const { PG_CONFIG } = require('../config/config.js');
-const db = pgp(PG_CONFIG);
+const config = require('../config/config.js');
+
+const db = pgp({
+  user: config.pgUser,
+  host: config.pgHost,
+  database: config.dbDatabase,
+  password: config.pgPassword,
+  port: config.pgPort,
+});
+
+//TODO: add on error handler
 
 module.exports = db;
