f7cb82c662f117becb28a9b30707b074efdadcb2
Andrew <Andrewkn09@Gmail.com>
2019-08-09 11:45:49 -0400

    refactor using pool

--- database/database.js
+++ database/database.js
@@ -1,13 +1,14 @@
-const pgp = require('pg-promise')();
+const { Pool } = require('pg');
 
 const config = require('../config/config.js');
 
-const db = pgp({
+const db = new Pool({
   user: config.pgUser,
   host: config.pgHost,
   database: config.dbDatabase,
   password: config.pgPassword,
   port: config.pgPort,
+  max: 10,
 });
 
 //TODO: add on error handler
