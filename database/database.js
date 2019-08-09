041141a9eef5a3137b9012fcd6dcf15950214315
Andrew <Andrewkn09@Gmail.com>
2019-08-09 11:39:33 -0400

    Add pool

--- database/database.js
+++ database/database.js
@@ -2,6 +2,15 @@ const { Pool } = require('pg');
 
 const config = require('../config/config.js');
 
+const db = new Pool({
+  user: config.pgUser,
+  host: config.pgHost,
+  database: config.dbDatabase,
+  password: config.pgPassword,
+  port: config.pgPort,
+  max: 10,
+});
+
 //TODO: add on error handler
 
 module.exports = db;
