9a2affbf6c735c68411bad2233494152b7aaa9b1
Andrew <Andrewkn09@Gmail.com>
2019-08-13 08:51:56 -0400

    Remove comments

--- server/models/questionModel.js
+++ server/models/questionModel.js
@@ -24,57 +24,7 @@ module.exports = {
         )
         .then(t.batch);
     });
-
-    return db
-      .any(
-        'SELECT question_id, question_body, question_date, asker_name, question_helpfulness FROM questions WHERE product_id = $1 AND reported = 0 LIMIT $2 OFFSET $3',
-        [product_id, count, offset]
-      )
-
-      .then(questions => {
-        return Promise.all(
-          questions.map(question => {
-            question.answers = {};
-            return db
-              .any(
-                'SELECT answer_id AS id, body, date, answerer_name, helpfulness, photos FROM answers WHERE question_id = $1 AND report = 0',
-                [question.question_id]
-              )
-              .then(answers => {
-                for (let answer of answers) {
-                  question.answers[answer.id] = answer;
-                }
-                return question;
-              });
-          })
-        );
-      });
   },
-  // getQuestions: (product_id, count, offset) => {
-  //   return db
-  //     .any(
-  //       'SELECT question_id, question_body, question_date, asker_name, question_helpfulness FROM questions WHERE product_id = $1 AND reported = 0 LIMIT $2 OFFSET $3',
-  //       [product_id, count, offset]
-  //     )
-  //     .then(questions => {
-  //       return Promise.all(
-  //         questions.map(question => {
-  //           question.answers = {};
-  //           return db
-  //             .any(
-  //               'SELECT answer_id AS id, body, date, answerer_name, helpfulness, photos FROM answers WHERE question_id = $1 AND report = 0',
-  //               [question.question_id]
-  //             )
-  //             .then(answers => {
-  //               for (let answer of answers) {
-  //                 question.answers[answer.id] = answer;
-  //               }
-  //               return question;
-  //             });
-  //         })
-  //       );
-  //     });
-  // },
   addQuestion: (product_id, { body, name, email }) => {
     return db.any(
       `INSERT INTO questions (product_id, question_body, asker_name, asker_email) VALUES ($1, $2, $3, $4)`,
